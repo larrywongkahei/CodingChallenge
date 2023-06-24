b7035b67a829c47b821c70f28256906e3b5956da
Maryhei <makemak123@gmail.com>
2023-06-24 11:47:00 +0100

    Added two test in main method

--- CodingChallengeTest/Program.cs
+++ CodingChallengeTest/Program.cs
@@ -66,7 +66,12 @@
 
     public static void Main(string[] args)
     {
-        int[] arrayToTest = new int[] { 1, 2, 3, 4, 5 };
+        //test1
+        //int[] arrayToTest = new int[] { 1, 2, 3, 4, 5 };
+        //int[][] newArray = groupArrayElements(arrayToTest, 3);
+        //Console.WriteLine(printNumbersOfArraysInArray(newArray));
+        //test2
+        int[] arrayToTest = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
         int[][] newArray = groupArrayElements(arrayToTest, 3);
         Console.WriteLine(printNumbersOfArraysInArray(newArray));
     }
